cd42b975a0d08072d596e89a7055492a39bf552b
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-05-23 17:09:11 +0900

    login required when viewing product -gun-

--- Carrot_Bunny/src/com/board/controller/BoardViewServlet.java
+++ Carrot_Bunny/src/com/board/controller/BoardViewServlet.java
@@ -2,14 +2,17 @@ package com.board.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.board.model.service.BoardService;
 import com.board.model.vo.Board;
+import com.member.model.vo.Member;
 
 /**
  * Servlet implementation class BoardViewServlet
@@ -31,14 +34,22 @@ public class BoardViewServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		int num = Integer.parseInt(request.getParameter("no"));
-		
-		Board b = new BoardService().selectBoard(num);
-		
-		request.setAttribute("board", b);
-	
-		request.getRequestDispatcher("/views/board/boardView.jsp")
-		.forward(request, response);	}
+		HttpSession session = request.getSession(false);
+		Member loginMember = (Member)session.getAttribute("loginMember");
+		if(session == null || loginMember == null) {
+			request.setAttribute("msg", "로그인 후 이용할 수 있습니다.");
+			request.setAttribute("loc", "/board/boardPage");
+			RequestDispatcher rd = request.getRequestDispatcher("/views/common/msg.jsp");
+			rd.forward(request, response);
+		} else {
+			int num = Integer.parseInt(request.getParameter("no"));
+			
+			Board b = new BoardService().selectBoard(num);
+			
+			request.setAttribute("board", b);
+			request.getRequestDispatcher("/views/board/boardView.jsp").forward(request, response);
+		}
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
