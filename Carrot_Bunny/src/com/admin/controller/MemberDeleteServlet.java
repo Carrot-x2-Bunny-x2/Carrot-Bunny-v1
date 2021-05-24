3b29a6821aa4537d0c9178aab6d4ffe031890a7c
sehong0429 <qkrdlgo1214@naver.com>
2021-05-24 22:02:21 +0900

    admin 회원 delete기능 push -sy-

--- Carrot_Bunny/src/com/admin/controller/MemberDeleteServlet.java
+++ Carrot_Bunny/src/com/admin/controller/MemberDeleteServlet.java
@@ -13,7 +13,7 @@ import com.admin.model.service.AdminService;
 /**
  * Servlet implementation class MemberDelete
  */
-@WebServlet("/deleteMember")
+@WebServlet("/admindeleteMember")
 public class MemberDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
