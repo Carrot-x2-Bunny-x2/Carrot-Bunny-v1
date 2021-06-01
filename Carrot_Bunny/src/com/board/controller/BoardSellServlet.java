bd61552ae4b42c7d29e8b085fb80b528c811a29e
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-06-01 12:44:26 +0900

    paging write and sell -gun-

--- Carrot_Bunny/src/com/board/controller/BoardSellServlet.java
+++ Carrot_Bunny/src/com/board/controller/BoardSellServlet.java
@@ -1,6 +1,7 @@
 package com.board.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.board.model.service.BoardService;
 import com.board.model.vo.Board;
+import com.board.model.vo.Comment;
+import com.love.model.service.LoveService;
+import com.love.model.vo.Love;
 
 /**
  * Servlet implementation class BoardSellServlet
@@ -31,12 +35,7 @@ public class BoardSellServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		int cPage;
-		try {
-			cPage=Integer.parseInt(request.getParameter("cPage"));
-		}catch(NumberFormatException e) {
-			cPage=1;
-		}
+		
 		
 		Board b = new Board();
 		b = new BoardService().selectBoard(Integer.parseInt(request.getParameter("no")));
@@ -53,19 +52,66 @@ public class BoardSellServlet extends HttpServlet {
 //		
 //		int result = new BoardService().sellBoard(b);
 		
-		String msg = "";
-		String loc = "/board/boardPage?cPage="+cPage;
 		if (result > 0) {
-			msg = "판매처리 완료";
+			System.out.println("판매 처리 완료");
+			int cPage;
+			try {
+				cPage=Integer.parseInt(request.getParameter("cPage"));
+			}catch(NumberFormatException e) {
+				cPage=1;
+			}
+			request.setAttribute("cPage", cPage);
+			int user;
+			try {
+				user=Integer.parseInt(request.getParameter("user"));
+			}catch(NumberFormatException e) {
+				user=0;
+			}
+			request.setAttribute("user", user);
+			int love;
+			try {
+				love=Integer.parseInt(request.getParameter("love"));
+			}catch(NumberFormatException e) {
+				love=0;
+			}
+			request.setAttribute("love", love);
+			int sold;
+			try {
+				sold=Integer.parseInt(request.getParameter("sold"));
+			}catch(NumberFormatException e) {
+				sold=0;
+			}
+			request.setAttribute("sold", sold);
+			String searchType;
+			try {
+				searchType=request.getParameter("searchType");
+			}catch(NumberFormatException e) {
+				searchType="";
+			}
+			request.setAttribute("searchType", searchType);
+			String keyword;
+			try {
+				keyword=request.getParameter("keyword");
+			}catch(NumberFormatException e) {
+				keyword="";
+			}
+			request.setAttribute("keyword", keyword);
+			List<Love> loveList = new LoveService().selectLoveList();
+			request.setAttribute("loveList",loveList);
+			
+			int num = Integer.parseInt(request.getParameter("no"));
+			Board board = new BoardService().selectBoard(num);
+			request.setAttribute("board", board);
+			
+			List<Comment> comments = new BoardService().selectComment(num);
+			request.setAttribute("comments", comments);
 		} else if (result == -1) {
-			msg = "이미 판매완료 된 상품입니다.";
+			System.out.println("이미 판매 처리된 상품");
 		} else {
-			msg = "판매처리 실패. 관리자에게 문의하십시오.";
+			System.out.println("판매 처리 실패");
 		}
 		
-		request.setAttribute("msg", msg);
-		request.setAttribute("loc", loc);
-		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
+		request.getRequestDispatcher("/views/board/boardView.jsp").forward(request, response);
 	}
 
 	/**
